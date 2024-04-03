8cf3a81bf6ab7b907c7e2039d17e479ec15c6d09
daw2-barrio23 <113451779+daw2-barrio23@users.noreply.github.com>
2024-04-03 18:57:27 +0000

    componente header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,7 +1,14 @@
 export function Header(){
     return(
-        <h1 className="text-4xl p-2 border-b-2">
-            Componente Header
-        </h1>
+        <div className="mx-auto p-3 items-center bg-gray-800 text-white">
+            <nav className="flex justify-center">
+                <ul className="flex space-x-4 color-white">
+                    <li><a className="border rounded p4 shadow" href="/">Home</a></li>
+                    <li><a className="border rounded p4 shadow" href="/">Juego</a></li>
+                    <li><a className="border rounded p4 shadow" href="/">A cerca de</a></li>
+                    
+                </ul>
+            </nav>
+        </div>
     )
 }
