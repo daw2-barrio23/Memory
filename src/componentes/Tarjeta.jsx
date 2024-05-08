b05fbd5ff56d25dd43d80051e425d08e71529e32
daw2-barrio23 <113451779+daw2-barrio23@users.noreply.github.com>
2024-05-08 18:11:59 +0000

    historia 6 tasca 1

--- src/componentes/Tarjeta.jsx
+++ src/componentes/Tarjeta.jsx
@@ -4,19 +4,31 @@ import { useState } from 'react';
 import { useClicksContext } from '../context/GlobalContext'; // Asegúrate de tener la ruta correcta
 
 export function Tarjeta({ nombre, imagen }) {
+  const [volteada, setVolteada] = useState(false);
   const [contadorClicks, setContadorClicks] = useState(0);
   const { incrementClicks } = useClicksContext();
 
   const handleClick = () => {
     setContadorClicks(contadorClicks + 1);
     incrementClicks();
+    setVolteada(true); // Voltear la tarjeta al hacer clic
+
+    setTimeout(() => {
+      setVolteada(false); // Volver a ocultar la tarjeta después de un segundo
+    }, 1000);
   };
 
   return (
-    <div className="bg-slate-200 rounded w-[150px] h-[220px] border p-2 shadow-lg text-center">
-      <p>Clicks: {contadorClicks}</p>
-      <img className="h-[150px]" src={imagen} onClick={handleClick} alt="Imagen" />
-      <h2 className="pt-1">{nombre}</h2>
+    <div className="bg-slate-200 rounded w-[150px] h-[220px] border p-2 shadow-lg text-center" onClick={handleClick}>
+      {volteada ? ( // Mostrar contenido de la tarjeta si está volteada
+        <>
+          <p>Clicks: {contadorClicks}</p>
+          <img className="h-[150px]" src={imagen} alt="Imagen" />
+          <h2 className="pt-1">{nombre}</h2>
+        </>
+      ) : ( // Mostrar la parte trasera de la tarjeta si no está volteada
+        <img className="h-[150px]" src="poke.png" alt="Parte trasera" />
+      )}
     </div>
   );
 }
