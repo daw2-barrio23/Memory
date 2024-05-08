ce13aaad1eb072a948ac98355885a254bd4e59c2
daw2-barrio23 <113451779+daw2-barrio23@users.noreply.github.com>
2024-05-08 17:47:04 +0000

    conexion pokeapi

--- src/componentes/GrupoTarjetas.jsx
+++ src/componentes/GrupoTarjetas.jsx
@@ -1,14 +1,64 @@
-import { arrayPersonajes } from "../bd";
-import { Tarjeta } from "./Tarjeta";
+// GrupoTarjetas.jsx
+
+import { useState, useEffect } from 'react';
+import { Tarjeta } from './Tarjeta';
 
 export function GrupoTarjetas() {
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      const pokemonIds = generateRandomPokemonIds(9); // Obtener 9 IDs aleatorias
+      const fetchedPokemonData = await Promise.all(pokemonIds.map(id => fetchPokemon(id)));
+      const duplicatedPokemonData = [...fetchedPokemonData, ...fetchedPokemonData]; // Duplicar la matriz de datos
+      const shuffledPokemonData = shuffleArray(duplicatedPokemonData); // Desordenar las tarjetas
+      setPokemonData(shuffledPokemonData);
+    };
+
+    fetchPokemonData();
+  }, []); // Ejecutar solo una vez al montar el componente
+
+  const generateRandomPokemonIds = (count) => {
+    const randomIds = [];
+    for (let i = 0; i < count; i++) {
+      randomIds.push(Math.floor(Math.random() * 898) + 1); // Generar IDs aleatorios entre 1 y 898 (el máximo de Pokémon)
+    }
+    return randomIds;
+  };
+
+  const fetchPokemon = async (id) => {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch Pokémon data');
+      }
+      const data = await response.json();
+      return {
+        id: data.id,
+        nombre: data.name,
+        imagen: data.sprites.front_default,
+      };
+    } catch (error) {
+      console.error('Error fetching Pokémon data:', error);
+    }
+  };
+
+  const shuffleArray = (array) => {
+    // Algoritmo de Fisher-Yates para desordenar el array
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
   return (
-    <div className="flex mx-auto flex-wrap bg-slate-599 gap-2 p-5"> {/* Cambio a gap-2 */}
-      {arrayPersonajes.map(personaje => (
+    <div className="flex mx-auto flex-wrap bg-slate-599 gap-2 p-5">
+      {pokemonData.map((pokemon, index) => (
         <Tarjeta
-          key={personaje.id}
-          nombre={personaje.nombre}
-          imagen={personaje.imagen}
+          key={index}
+          nombre={pokemon.nombre}
+          imagen={pokemon.imagen}
         />
       ))}
     </div>
